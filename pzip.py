97f59fff9b51ff25fb58aad1469dc837b9877c30
andre <andrenunes103@gmail.com>
2017-10-19 18:04:48 +0100

    finished argparse

--- pzip.py
+++ pzip.py
@@ -13,21 +13,52 @@
 
 import sys
 from zipfile import ZipFile
+import argparse
 
 __author__ = "André Nunes 43304, Miguel Almeida 48314, Tiago Martins 48299"
 
 if __name__ == '__main__':
     args = sys.argv[1:]
 
-try:
-    if args[0] == '-c':
-        print "Compress"
+# Combinacoes possivies
+# -c -p n -t {files}
+# -c -p n {files}
+# -c -t {files}
+# -c {files}
 
-    elif args[0] == '-d':
-        print "Descompress"
+# try:
+#     if args[0] == '-c':
+#         print "Compress"
 
-    else:
-        print "Choose an option: -c | -d"
+#     elif args[0] == '-d':
+#         print "Descompress"
 
-except Exception as e:
-    print "Error:", e
+#     else:
+#         print "Choose an option: -c | -d"
+
+# except Exception as e:
+#     print "Error:", e
+
+parser = argparse.ArgumentParser()
+
+group = parser.add_mutually_exclusive_group()
+group.add_argument("-c", help="Compress files", action="store_true")
+group.add_argument("-d", help="Decompress files", action="store_true")
+
+parser.add_argument("-p", type=int, choices=range(10), help="Number os process")
+parser.add_argument("-t", help="Finish whem file not found", action="store_true")
+parser.add_argument("files", nargs='*', help="Files to Compress/Decompress")
+
+args = parser.parse_args()
+filesnames = []
+
+if args.files == []:
+    filename = raw_input("qual o nome do ficheiro?\n")
+
+    while len(filename) != 0:
+        filesnames.append(filename)
+        filename = raw_input("qual o nome do ficheiro?\n")
+else:
+    filesnames = args.files
+
+print "python pzip.py", args, "filesnames", filesnames
